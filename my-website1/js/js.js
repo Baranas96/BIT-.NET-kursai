4038a95f9e59c30089987c4651a5074c92ce139f
Mantas Baranauskas <mantas.baranauskas@hotmail.com>
2021-12-19 08:51:01 +0200

    about page and news page with content

---
+++ my-website1/js/js.js
@@ -0,0 +1,13 @@
+//  FUNCTION TO PRINT THE H1 TEXT
+// function printLetterByLetter(destination, message, speed){
+//     var i = 0;
+//     var interval = setInterval(function(){
+//         document.getElementById(destination).innerHTML += message.charAt(i);
+//         i++;
+//         if (i > message.length){
+//             clearInterval(interval);
+//         }
+//     }, speed);
+// }
+
+// printLetterByLetter("someElement", "Hello world, bonjour le monde.", 100);
